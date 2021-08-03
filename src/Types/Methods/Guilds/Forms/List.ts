812b49d7cf3061b935ac715d22da56c377694342
Chris Talman <cewe.mail@gmail.com>
2021-08-03 11:04:19 +0100

    Fixed parameters.

--- src/Types/Methods/Guilds/Forms/List.ts
+++ src/Types/Methods/Guilds/Forms/List.ts
@@ -7,8 +7,6 @@ import { RequestOptionsWrapper } from '../../../Methods';
 // Method
 export interface MethodParameters extends RequestOptionsWrapper, Pick <ApiDataForms.GetRequestBody, 'pluck' | 'pagination'>
 {
-	id: string;
-	formId: string;
 	guildId: string;
 	filters?: ApiDataForms.GetRequestBodyDataFilters;
 	sort?: ApiDataForms.GetRequestBodyDataSort;
