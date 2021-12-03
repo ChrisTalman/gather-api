dba5ce44fe1639e826693efd4b3cfe7bf1f55726
Chris Talman <cewe.mail@gmail.com>
2021-12-03 17:53:45 +0000

    Introduced `active` and made `premium` optional.

--- src/Types/Api/Data/Guilds/index.ts
+++ src/Types/Api/Data/Guilds/index.ts
@@ -5,9 +5,10 @@ export interface Guild
 	id: string;
 	name: string;
 	logo: string;
-	premium: GuildPremium;
+	premium?: GuildPremium;
 };
 export interface GuildPremium
 {
 	current: boolean;
+	active: boolean;
 };
